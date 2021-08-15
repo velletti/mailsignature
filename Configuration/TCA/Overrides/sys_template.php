c700b078a6d24e7eab29117ae28ca2c16214c671
Joerg Velletti <jvelletti@allplan.com>
2021-08-15 20:46:10 +0200

    [LTS10] First LTS 10 adjustments

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,5 +1,4 @@
 <?php
 defined('TYPO3_MODE') or die();
-$_EXTKEY = 'mailsignature' ;
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Mail Signature Service');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('mailsignature', 'Configuration/TypoScript', 'Mail Signature Service');
